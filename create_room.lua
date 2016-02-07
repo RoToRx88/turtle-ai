e0af05ba81a3378a0258c6670c63a1acbcaaa9d0
RoToRx88 <comte_a@epitech.eu>
2016-02-07 23:34:15 +0100

    initial commit

---
+++ create_room.lua
@@ -0,0 +1,58 @@
+write("x (front) :")
+local size_x = tonumber(read())
+write("y (right) : ")
+local size_y = tonumber(read())
+write("z (top) : ")
+local size_z = tonumber(read())
+
+turtle.refuel()
+for z = 1, size_z do
+	local go_forward = true
+
+	for i = 1, size_y do
+		for x = 1, size_x - 1 do
+			turtle.dig()
+			turtle.forward()
+		end
+		if (go_forward and i < size_y) then
+			turtle.turnRight()
+			turtle.dig()
+			turtle.forward()
+			turtle.turnRight()
+			go_forward = false
+		elseif (not go_forward and i < size_y) then
+			turtle.turnLeft()
+			turtle.dig()
+			turtle.forward()
+			turtle.turnLeft()
+			go_forward = true
+		end
+	end
+	if (go_forward) then
+		turtle.turnRight()
+		turtle.turnRight()
+		for i = 2, size_x  do
+			turtle.forward()
+		end
+		turtle.turnRight()
+		for i = 2, size_y do
+			turtle.forward()
+		end
+		turtle.turnRight()
+	elseif (not go_forward) then
+		go_forward = true
+		turtle.turnRight()
+		for i = 2, size_y do
+			turtle.forward()
+		end
+		turtle.turnRight()
+	end
+	if (z < size_z) then
+		turtle.digUp()
+		turtle.up()
+	else
+		for z_back_home = 2, size_z do
+			turtle.down()
+		end
+	end
+end
